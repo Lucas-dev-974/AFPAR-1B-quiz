6c257e8dd56036bd08cc729dca8d1480c2b54c06
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-05 10:49:37 +0100

    Ajout d'un model salarié

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from .managers import *
+
+# On instancie ici un model salarié qui serat les utilisateurs de l'app
+class Salarie(AbstractUser):
+    username = None
+    email = models.EmailField('email', unique=True)
+    role  = models.CharField('role', default='salarié', max_length=50)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = SalarieManager()
+
+    def __str__(self):
+        return self.email
+
