a85adc31a5d3e7b39714fc8b5b4100e8947f77e8
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-10 05:30:24 +0100

    mise à jour du lecteur de fichier avec l'enregistrement en bdd des entité Salarie, Question, Reponse, ReponseLierAQuestion

--- backend/lector.py
+++ backend/lector.py
@@ -4,87 +4,130 @@ O. crée un fichier texte avec tous les qcm (trame générale)
 I. exploite une série de fichiers XML, structure à decouvrir
 """
 import xml.etree.ElementTree as ET
-from os import walk 
+from .models import *
+
+def saveQuizz(fields):
+    quizz = Quizz.objects.create(
+        nom_quizz = fields['quizz_name'],
+        idsession_id = fields['sessionid'],
+        id_sa_id = fields['secteurid']
+    )
+
+    return quizz
+
+def linkQuestionToQuizz(fields): 
+    linked_quizz_session = Possede.objects.create(
+        idquizz_id    = fields['quizzid'],
+        idquestion_id = fields['questionid']
+    )
 
 
-# https://courspython.com/classes-et-objets.html
-# https://www.journaldunet.fr/web-tech/developpement/1499365-comment-creer-un-nouveau-fichier-text-txt-en-python/
-# https://bobbyhadz.com/blog/python-unicodeencodeerror-charmap-codec-cant-encode-characters-in-position
-class Texte:
-    def __init__(self,txt=''):
-        self.text = txt 
-    def add(self, txtAdd):
-        self.text += txtAdd + '\n'
-    def createLog(self, ficLog) : 
-        file = open(ficLog, "w", encoding='iso-8859-1')
-        file.write(self.text) 
-        file.close()
-    def console(self) :
-        print('\n\n'+self.text)
+def saveQuestion(fields):
+    question = Questions.objects.create(
+        intitule_question=fields['intituler']
+    )
+    return question
 
-# https://www.journaldunet.fr/web-tech/developpement/1202869-comment-lister-tous-les-fichiers-d-un-repertoire-en-python/
-def  getListeFichiers(dossier) :
-    listeFichiers = []
-    for (repertoire, sousRepertoires, fichiers) in walk(dossier):
-        listeFichiers.extend(fichiers)
-        break                             # donc niv1 seulement
-    # print(listeFichiers)
-    return listeFichiers
+def saveResponseOption(fields):
+    reponse =  ReponsesPropose.objects.create(
+        intitule_reponse=fields['intituler'],
+        bonne_reponse=fields['bonne']
+    )
+
+    return reponse
+
+def linkResponseToQuestion(responseid, questionid):
+    link_response_question = ReponseALaQuestion.objects.create(idquestion_id = questionid, idreponse_id = responseid)
+    return link_response_question
 
 
-def traiterUnFichier( fichierXml ) : 
+def saveSalarie(fields):
+    salarie = Salarie.objects.create(
+        email = fields['email'],
+        nom   = fields['nom'],
+        prenom = fields['prenom'],
+        matricul = fields['matricul'],
+    )
+
+    salarie.set_password(fields['password'])
+    salarie.save()
+
+def getQuestionnaire(file, file_name=""):
+    # Instanciation de Element Tree pour pouvoir lire le fichier si fichier XML
+    tree = ET.parse(file)
+    XMLroot = tree.getroot()
+
+    # Créer une entité quizz qui à pour nom le nom du fichier 
+    quizz = saveQuizz({
+        "quizz_name": str(file),
+        "sessionid": 1,
+        "secteurid": 1
+    })
+
     
-    mytree = ET.parse( fichierXml )
-    myroot = mytree.getroot()
-        
-    # debut de fichier
-    noq=0 
-    ret = Texte()
+    # Récupère toutes les question présent dans le fichier
+    questions = XMLroot.findall('question')
 
-    # boucle de traitement des X elements du fichier
-    for question in myroot:
-        # debut de question
-        noq+= 1
-        ret.add( 'Question ' + str(noq) + ' ---------------------------------------')
-        ret.add( question.find('intitule').text )
+    #Pour toute les question du fichier 
+    for question in questions:
 
+        # On récupère les informations du questionnaire
         bonne = question.attrib.get('bonne')
-        compteur = 1
-
-        # boucle de traitement 
-        for reponse in question.find('listerep'):
-            if compteur == int(bonne):
-                ret.add(' [ * ] ' + reponse.text )
-            else:
-                ret.add(' [ ] ' + reponse.text ) 
-
-            compteur += 1
+        titre = question.findtext('titre')
+        intituler = question.findtext('intitule')
         
-        # fin de question
-        ret.add('feedback : ' + question.find('feedback').text )
-        ret.add('')
+        # On créer ici une entité question 
+        question_ = saveQuestion({
+            'titre': titre,
+            'intituler': intituler
+        })
+
+        # Puis on créer une entité qui est le lien entre la question et le quizz (entité possede)
+        linked_question_to_quizz = linkQuestionToQuizz({
+            "quizzid": quizz.pk,
+            "questionid": question_.pk
+        })
+
+        # On vas ensuite récupérer les réponse possible 
+        reponses = question.find('listerep').findall('reponse')
+        
+        for (idx, reponse) in enumerate(reponses):
+            bonne_ = None
+            if(idx + 1 == int(bonne)):
+                bonne_ = True
+            else:
+                bonne_ = False
+
+            reponse_ = saveResponseOption({
+                'intituler': reponse.text,
+                'bonne': bonne_
+            })
+
+            linkResponseToQuestion(questionid=question_.pk, responseid=reponse_.pk)
+
+def getSalaries(file):
+    _file = file.read().decode('utf-8').splitlines()
+    print(_file[1].split(';'))
+
+
+    for user_infos in _file:
+        infos = user_infos.split(';')
+        secteur = infos[0]
+        secteur_name = infos[1]
+        matricule_salarie = infos[5]
+        salarie_name = infos[6]
+        salarie_last_name = infos[7] 
+        salarie_naissance = infos[8]
+        salarie_code_postal = infos[12]
+
+        saveSalarie({
+            'email': matricule_salarie + '@gmail.com',
+            'nom': salarie_name,
+            'prenom': salarie_last_name,
+            'matricul': matricule_salarie,
+            'password': matricule_salarie + '1234'
+        })
+
     
-    # fin du fichier
-    return ret.text
-
-
-monRepertoire = './questionnaires/'
-listeFichiers = getListeFichiers(monRepertoire)
-
-""" boucle pour traiter chaque fichier trouvé
-"""
-out = Texte()
-out.add ('RESULTATS\n' ) 
-
-#for fich in listeFichiers[0:3] :    # pour test!
-for fich in listeFichiers :
-    out.add ( '#########################################################################')
-    out.add ( '###  QUESTIONNAIRE : ' + fich )
-    out.add ( '#########################################################################')
-    print(fich)    # verbose
-    out.add ( traiterUnFichier( monRepertoire + fich ) )
-
-out.createLog('./questionnaires_v0.txt') 
-#out.console()
-
-print( str(len(listeFichiers)) + ' fichiers traités' )
+    # for row in reader:
+    #     print(row)
