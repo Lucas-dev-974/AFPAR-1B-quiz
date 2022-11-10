c2631eded00cbc6a972f5f2295fb3815ddd672d5
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-10 05:29:10 +0100

    Ajout d'urls

--- backend/urls.py
+++ backend/urls.py
@@ -4,5 +4,13 @@ from backend.Views import *
 
 urlpatterns = [
     path('login', AuthView.as_view()),
-    path('helloworld', HelloView.as_view())
+
+
+    path('question/imports', HelloView.as_view()),
+    path('question', Question.as_view()),
+    path('question', Question.as_view()), 
+    
+    path('salaries/import', Salarie.as_view()),
+    
+    path('quizz', QuizzView.as_view())
 ]
