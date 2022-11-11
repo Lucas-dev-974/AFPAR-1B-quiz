77def046b6d85fde042e9720f7e99ef8b7af0cba
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-11 18:20:53 +0100

    Ajout d'un Serializer pour le login

--- backend/Formulaire/index.py
+++ backend/Formulaire/index.py
@@ -1,32 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
-
-class LoginForm(forms.Form):
-    email = forms.CharField(
-        label='Nom utilisateur', 
-        max_length=70, 
-        required=True,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'id': 'username_input'
-            }
-        )
-    )
-    
-    password = forms.CharField(
-        label='Mot de passe', 
-        max_length=100, 
-        required=True,
-        widget = forms.PasswordInput(
-            attrs={
-                'class': 'form-control',
-                'id': 'password_input'
-            }
-        ), 
-    )
-
+from rest_framework import serializers
 
 class UploadFileForm(forms.Form):
     mode = forms.CharField(max_length=50)
     file = forms.FileField()
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=100, required=True)
+    password = serializers.CharField(max_length=100, required=True)
