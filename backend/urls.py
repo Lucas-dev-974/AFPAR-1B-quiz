e0c913735285f5f7b14baa61b877eed2f1d69c92
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-11 14:27:05 +0100

    Url ajout de /api/token/check | /api/salarie/import

--- backend/urls.py
+++ backend/urls.py
@@ -4,13 +4,13 @@ from backend.Views import *
 
 urlpatterns = [
     path('login', AuthView.as_view()),
-
+    path('token/check', TestToken.as_view()),
 
     path('question/imports', HelloView.as_view()),
     path('question', Question.as_view()),
     path('question', Question.as_view()), 
     
-    path('salaries/import', Salarie.as_view()),
+    path('salaries/import', SalarieView.as_view()),
     
     path('quizz', QuizzView.as_view())
 ]
