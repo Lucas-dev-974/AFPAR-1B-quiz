3268e8ab81a8110e41a9695f075097be91d87b37
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-16 10:02:57 +0100

    Ajout d'une view pour récuperer tout les quizzs à passer pour x salarié

--- backend/Views/QuizzView.py
+++ backend/Views/QuizzView.py
@@ -22,7 +22,6 @@ def getQuestionWithResponseOptions(questionid):
     linked_responses = ReponseALaQuestion.objects.filter(idquestion = questionid)
 
     for link_reponse in linked_responses:
-        print(link_reponse.pk)
         reponse = ReponsesPropose.objects.get(pk=link_reponse.pk)
 
         question_reponse['reponses'].append({
@@ -31,6 +30,31 @@ def getQuestionWithResponseOptions(questionid):
         })
         
     return question_reponse 
+
+def quizzForUser(userid):
+    user_participation = Participe.objects.filter(userid_id = userid)
+    quizzs = []
+
+    for participation in user_participation:
+        _quizzs = Quizz.objects.filter(
+            idsession = participation.idsession
+        )   
+        
+        for quizz in _quizzs:
+            _quizz = {}
+            _quizz['id'] = quizz.pk
+            _quizz['nom'] = quizz.nom_quizz
+
+            _quizz['questions'] = []
+            possede_questions = Possede.objects.filter(idquizz = quizz.pk)
+
+            for posseder in possede_questions:
+                question_id = posseder.idquestion_id
+                _quizz['questions'].append(getQuestionWithResponseOptions(question_id))
+            quizzs.append(_quizz)
+
+        return quizzs
+
 
 def getQuizz(quizz_id):
     quizz = {}
@@ -67,45 +91,62 @@ class Question(APIView):
             # Pour tous les fichiés présent dans la requête on en récuperent les questionnaires //
             for filename, file in request.FILES.items():
                 getQuestionnaire(file)
-        return JsonResponse({"ok": 'ok'})
+        return JsonResponse({"status": 'quizz importer avec succès'})
 
+def getParticipantsForSession(session_id):
+    participants = Participe.objects.filter(idsession = session_id)
+    print(participants)
 
 class QuizzView(APIView):
     # Si le client n'a pas fourni de Token de connexion alor une erreur 401 unauthorized lui est retourner
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        if(request.GET.get('quizzid') is None): return JsonResponse({'status': 'Le paramètre quizzid dois être renseigner dans l\'uri de la requête !'})
-        quizzid = request.GET.get('quizzid')    
-        if(quizzid.isdigit() == False): return JsonResponse({'status': 'L\'id dois être un nombre entier!'}, 400)
+        if('quizzs' in request.path):
+            if isAdmin(request) == False: return JsonResponse({'status': 'Vous devez être DSI ou DRH pour importer de nouveaux salariés'}, status = 401)
+            _quizzs = []
+            quizzs = Quizz.objects.all()
 
-        quizz_data = {}
 
-        quizz = Quizz.objects.get(pk=quizzid)
-        possede_questions = Possede.objects.filter(idquizz = quizz.pk)
-        
-        quizz_data['quizzid']   = quizz.pk
-        quizz_data['nom']       = quizz.nom_quizz
-        quizz_data['questions'] = []
+            for quizz in quizzs:
+                _quizz = {}
+                _quizz['nom'] = quizz.nom_quizz
+                _quizz['id']  = quizz.pk
+                _quizz['in_session'] = {
+                    'session_id': quizz.idsession.id,
+                    'date_de_deploiement': quizz.idsession.date_de_deploiment
+                }
 
-        for posseder in possede_questions:
-            question_id = posseder.idquestion_id
-            quizz_data['questions'].append( getQuestionWithResponseOptions(question_id))
+                _quizz['participants'] = Participe.objects.filter(idsession = quizz.idsession.id).count()
 
-        return JsonResponse(quizz_data)
 
+                possede_questions = Possede.objects.filter(idquizz = quizz.pk)
+
+                questions = []
+
+                for posseder in possede_questions:
+                    question_id = posseder.idquestion_id
+                    questions.append(getQuestionWithResponseOptions(question_id))
+                
+                _quizz['questions'] = questions
+                _quizzs.append(_quizz)
+            
+            return JsonResponse(_quizzs, safe=False)
+
+        else: 
+            return JsonResponse(quizzForUser(request.user.id), safe=False)
     
     def post(self, request):
         quizz = request.data
-        
+        # return JsonResponse({"status": 1})
         for reponse in quizz['reponses']:
             histo_reponse_quizz = HistoriqueReponsesSelectionner.objects.create(
                 matricule_salarie_id = request.user.id,
-                idquizz_id = quizz['quizzid'],
-                idreponse_id = reponse['reponseid'],
+                idquizz_id    = quizz['quizzid'],
+                idreponse_id  = reponse['reponseid'],
                 idquestion_id = reponse['questionid']
             )
-            print(histo_reponse_quizz)
+            
         return JsonResponse(quizz, safe=False)
     
 
@@ -118,16 +159,24 @@ class QuizzImport(APIView):
     
         if('imports' in request.path): 
             form = UploadFileForm(request.POST, request.FILES)
-
+            print(request.FILES)
+            
             if form.is_valid():
                 for filename, file in request.FILES.items():
                     getQuestionnaire(file)
+
+
             return JsonResponse({"ok": 'ok'})
         
         if('creer' in request.path):
-            print(request.data)
+            
             session = Sessions.objects.create(date_de_deploiment = request.data['session_deploiment'])
             quizz   = Quizz.objects.create(nom_quizz = request.data['quizz_name'], idsession_id = session.pk, id_sa_id = 1)
+
+            participants= request.data['participants']
+
+            for participant in participants:
+                part = Participe.objects.create(userid_id = participant, idsession_id = session.id)
 
             for question in request.data['quizz_questions']:
                 question_ = Questions.objects.create(intitule_question = question['intitule'])
